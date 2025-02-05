7b95cdcac2c054a1aa1486f43415f2f15e330d9c
Ipeross2207 <ipeross2207@g.educaand.es>
2025-02-05 10:01:54 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import PerformanceChart from './screens/PerformanceChart';
+import ClassLayout from './screens/ClassLayout';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Class" component={ClassLayout} />
+                <Tab.Screen name="Performance" component={PerformanceChart} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
---
+++ src/screens/PerformanceChart.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PerformanceChart() {
+  return (
+    <View>
+      <Text>PerformanceChart</Text>
+    </View>
+  )
+}
